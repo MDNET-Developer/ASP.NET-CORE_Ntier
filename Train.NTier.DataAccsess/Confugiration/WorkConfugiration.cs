eb9f5c6ed5439f2632b23aa7bbfb870f026e2ae0
MDNET-Developer <qarabagli.murad@mail.ru>
2022-11-05 10:21:44 +0400

    update

--- Train.NTier.DataAccsess/Confugiration/WorkConfugiration.cs
+++ Train.NTier.DataAccsess/Confugiration/WorkConfugiration.cs
@@ -14,6 +14,7 @@ namespace Train.NTier.DataAccsess.Confugiration
         public void Configure(EntityTypeBuilder<Work> builder)
         {
             builder.HasKey(i => i.Id);
+            
             builder.Property(x => x.Description).HasMaxLength(350);
             builder.Property(y => y.Description).IsRequired(true);
             builder.Property(o => o.IsCompleted).IsRequired(true);
--- Train.NTier.Entity/Concrete/Work.cs
+++ Train.NTier.Entity/Concrete/Work.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Train.NTier.Entity.Interface;
 
 namespace Train.NTier.Entity.Concrete
 {
-    public class Work
+    public class Work: IEntity
     {
         public int Id { get; set; }
         public string Description { get; set; }
